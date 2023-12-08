af9e961b44d9e84cb79df456d9455b04cd9c5584
SHIV KUAMR SINGH <shiv01.chemengg@gmail.com>
2023-12-08 21:17:11 +0530

    part 2 with login sigup page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Home></Home>
+      </>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <>
+        <LoginPage></LoginPage>
+      </>
+    ),
+  },
+  {
+    path: "/signup",
+    element: (
+      <>
+        <SignupPage></SignupPage>
+      </>
+    ),
+  },
+]);
 
 const App = () => {
   return (
     <>
-      <Home></Home>
+      <RouterProvider router={router} />
     </>
   );
 };
