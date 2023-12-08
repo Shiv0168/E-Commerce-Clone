22e65656367dc8494b24bd0886975c8114cc5368
SHIV KUAMR SINGH <shiv01.chemengg@gmail.com>
2023-12-08 20:44:07 +0530

    part 1 with navbar and product list

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import Counter from "./features/counter/Counter";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
-      <Counter></Counter>
+      <Home></Home>
     </>
   );
 };
