1b155e601e042fd0513661e87c614fa28bf1f18f
SHIV KUAMR SINGH <shiv01.chemengg@gmail.com>
2023-12-10 14:02:52 +0530

    part 9 brands and category api added

--- Frontend/src/features/product/productSlice.js
+++ Frontend/src/features/product/productSlice.js
@@ -1,10 +1,17 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchAllProducts, fetchProductsByFilters } from "./productAPI";
+import {
+  fetchAllProducts,
+  fetchBrands,
+  fetchCategories,
+  fetchProductsByFilters,
+} from "./productAPI";
 
 const initialState = {
   products: [],
   status: "idle",
   totalItems: 0,
+  categories: [],
+  brands: [],
 };
 
 export const fetchAllProductsAsync = createAsyncThunk(
@@ -19,6 +26,21 @@ export const fetchProductsByFiltersAsync = createAsyncThunk(
   "product/fetchProductsByFilters",
   async ({ filter, sort, pagination }) => {
     const response = await fetchProductsByFilters(filter, sort, pagination);
+    return response.data;
+  }
+);
+
+export const fetchCategoriesAsync = createAsyncThunk(
+  "product/fetchCategories",
+  async () => {
+    const response = await fetchCategories();
+    return response.data;
+  }
+);
+export const fetchBrandsAsync = createAsyncThunk(
+  "product/fetchBrands",
+  async () => {
+    const response = await fetchBrands();
     return response.data;
   }
 );
@@ -47,6 +69,20 @@ export const productSlice = createSlice({
         state.status = "idle";
         state.products = action.payload.products;
         state.totalItems = action.payload.totalItems;
+      })
+      .addCase(fetchCategoriesAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchCategoriesAsync.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.categories = action.payload;
+      })
+      .addCase(fetchBrandsAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchBrandsAsync.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.brands = action.payload;
       });
   },
 });
@@ -54,6 +90,8 @@ export const productSlice = createSlice({
 export const { increment } = productSlice.actions;
 
 export const selectAllProducts = (state) => state.product.products;
+export const selectCategories = (state) => state.product.categories;
+export const selectBrands = (state) => state.product.brands;
 export const selectTotalItems = (state) => state.product.totalItems;
 
 export default productSlice.reducer;
